ba9825ab51422a141782c9987ecf3a04a92616dd
abd-alghani-soud <abdalghanisoud21@example.com>
2024-12-06 21:29:18 +0300

    finish bottem sheet

--- lib/views/widgets/add_show_model_bottem_sheeet.dart
+++ lib/views/widgets/add_show_model_bottem_sheeet.dart
@@ -7,6 +7,74 @@ class AddShowModelBottemSheeet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: ListView(
+        children: [
+          const SizedBox(
+            height: 45,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 8,
+                  vertical: 24.0,
+                ),
+                hintText: "title",
+                hintStyle: TextStyle(
+                  color: Colors.lightBlueAccent,
+                ),
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 8,
+                  vertical: 96.0,
+                ),
+                hintText: "content",
+                hintStyle: TextStyle(
+                  color: Colors.lightBlueAccent,
+                ),
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 45,
+          ),
+          Container(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextButton(
+                style: ButtonStyle(
+                  shape: WidgetStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  backgroundColor:
+                      WidgetStateProperty.all(Colors.lightBlueAccent),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  "Add",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
