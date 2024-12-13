e50bf58ae770bd4cd8236b17169804d18c135b8c
abd-alghani-soud <abdalghanisoud21@example.com>
2024-12-13 23:18:56 +0300

    add AddNote cubit to add note bottem sheet

--- lib/views/widgets/add_show_model_bottem_sheeet.dart
+++ lib/views/widgets/add_show_model_bottem_sheeet.dart
@@ -1,17 +1,35 @@
 // ignore_for_file: avoid_unnecessary_containers, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_app/cubits/add_notes_cubits/add_notes_cubit.dart';
 import 'package:note_app/views/widgets/add_note_form.dart';
-
 class AddShowModelBottemSheeet extends StatelessWidget {
   const AddShowModelBottemSheeet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: SingleChildScrollView(
-        child: AddNoteForm(),
+        child: BlocConsumer<AddNotesCubit, AddNotesState>(
+          listener: (context, state) {
+            if (state is AddNotesSuccess) {
+              Navigator.pop(context);
+            }
+            if (state is AddNotesFailure) {
+              // ignore: avoid_print
+              print('failed ${state.errorMessage}');
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNotesLoading ? true : false,
+              child: const AddNoteForm(),
+            );
+          },
+        ),
       ),
     );
   }
