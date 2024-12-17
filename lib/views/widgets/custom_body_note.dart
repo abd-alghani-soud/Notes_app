3f5740a723f8c1203d21cf084105913b74c186e4
abd-alghani-soud <abdalghanisoud21@example.com>
2024-12-17 23:36:06 +0300

    display notes 1

--- lib/views/widgets/custom_body_note.dart
+++ lib/views/widgets/custom_body_note.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/notes/notes_cubit.dart';
 import 'package:note_app/views/widgets/custom_app_bar.dart';
 import 'package:note_app/views/widgets/custom_list_view.dart';
 
-class CustomBodyNote extends StatelessWidget {
+class CustomBodyNote extends StatefulWidget {
   const CustomBodyNote({super.key});
+
+  @override
+  State<CustomBodyNote> createState() => _CustomBodyNoteState();
+}
+
+class _CustomBodyNoteState extends State<CustomBodyNote> {
+  @override
+  void initState() {
+    BlocProvider.of<NotesCubit>(context).fetchAllNote();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
